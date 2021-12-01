6a165ef3a8f35b8f39a906cbd931bfeeba804991
Nicolai Almskou <almskoun@hotmail.com>
2021-12-01 10:57:55 +0100

    Added local angle for receiver
    Signed-off-by: Nicolai Almskou <almskoun@hotmail.com>

--- Quadriga/get_data.m
+++ Quadriga/get_data.m
@@ -2,25 +2,26 @@
 function done = get_data(fc, pos_log, name, ENGINE, scenarios)
     load(pos_log);
     l = qd_layout;                                          % Create new QuaDRIGa layout
-    l.simpar.center_frequency = fc;                        % Set center frequency to 2 GHz
+    l.simpar.center_frequency = fc;                         % Set center frequency to 2 GHz
     l.simpar.use_absolute_delays = 1;                       % Enables true LOS delay
     l.simpar.show_progress_bars = 1;                        % Disable progress bars
     l.tx_position = [0, 0, 10]';                            % Set BS posittions
     
-    l.rx_track = qd_track();           % 50 m long track going north
-    %  'circular' , 100, pi/2
-    % l.rx_track.initial_position = [0 ; 15 ; 1.5];
+    l.rx_track = qd_track();
+
+    sp = size(pos_log);
+    AoA = cell(sp(1),1);
+    AoD = cell(sp(1),1);
+    Coeff = cell(sp(1),1);
+    Ori = cell(sp(1), 1);
     
-    AoA = cell(size(pos_log)(1),1);
-    AoD = cell(size(pos_log)(1),1);
-    Coeff = cell(size(pos_log)(1),1);
-    
-    for episode = 1:size(pos_log)(1)
-    
-        l.rx_track.positions=squeeze(pos_log(episode, :, :));         % Set start position and MT height
-        % l.rx_track.interpolate_positions(5);                   % One channel sample every 10 cm
-        sce_index = randi([1,size(scenarios)(2)]);
-        l.rx_track.scenario = scenarios(sce_index);           % Set propagation scenario
+    sc = size(scenarios);
+
+    for episode = 1:sp(1)
+        l.rx_track.positions=squeeze(pos_log(episode, :, :));   % Set start position and MT height
+        sce_index = randi([1, sc(1)]);
+        l.rx_track.scenario = strtrim(scenarios(sce_index, :));             % Set propagation scenario
+        l.rx_track.calc_orientation();
 
         l.rx_track.no_segments = l.rx_track.no_snapshots;       % Use spatial consisteny for mobility
 
@@ -33,16 +34,19 @@ function done = get_data(fc, pos_log, name, ENGINE, scenarios)
         c = merge( c, [], 0 );                          % Combine output channels
         c.individual_delays = 0;                        % Remove per-antenna delays
         
-        AoA(episode) = b.AoA;
-        AoD(episode) = b.AoD;
-        Coeff(episode) = squeeze(c.coeff(1,1,:,:))';
+        AoA{episode} = b.AoA;
+        AoD{episode} = b.AoD;
+        Coeff{episode} = squeeze(c.coeff(1,1,:,:))';
+        Ori{episode} = l.rx_track.orientation();
         
     end
     
-    output = cell(3,1);
+    output = cell(4,1);
     output{1} = AoA;
     output{2} = AoD;
     output{3} = Coeff;
+    output{4} = Ori;
+
     
     if ENGINE == "octave"
         save("-7", name, 'output');
--- main.py
+++ main.py
@@ -13,13 +13,13 @@ import helpers
 
 # %% Global Parameters
 RUN = True
-ENGINE = "octave"  # "octave" OR "MATLAB"
+ENGINE = "MATLAB"  # "octave" OR "MATLAB"
 
 # %% main
 if __name__ == "__main__":
 
     # Number of steps in a episode
-    N = 100
+    N = 50
 
     # Radius for commuication range [m]
     r_lim = 200
@@ -28,17 +28,17 @@ if __name__ == "__main__":
     stepsize = [0.5, 5]
 
     # Number of episodes
-    M = 5
+    M = 2
 
     # Number of antennae
-    Nt = 40  # Transmitter
+    Nt = 10  # Transmitter
     Nr = 10  # Receiver
 
     fc = 28e9  # Center frequncy
     lambda_ = 3e8 / fc
-    
+
     # Possible scenarios
-    scenarios = ['3GPP_38.901_UMi_LOS','3GPP_38.901_UMi_NLOS']
+    scenarios = ['3GPP_38.901_UMi_LOS']  # '3GPP_38.901_UMi_NLOS'
 
     t_start = time()
     # Load or create the data
@@ -50,11 +50,10 @@ if __name__ == "__main__":
 
     # Extract data
     print("Extracting data")
-    AoA = tmp[0][0]  # Angle of Arrival
-    AoD = tmp[1][0]  # Angle of Depature
+    AoA_Global = tmp[0][0]  # Angle of Arrival
+    AoD_Global = tmp[1][0]  # Angle of Depature
     coeff = tmp[2][0]  # Channel Coeffiecents
-
-    AoA_L = np.abs(np.arctan2(pos_log[0][1], pos_log[0][0]) * 180/np.pi)
+    Orientation = tmp[3][0]  # Orientation
 
     print("Starts calculating")
     # Make ULA - Transmitter
@@ -66,9 +65,10 @@ if __name__ == "__main__":
     r_t[0, :] = np.linspace(0, (Nt - 1) * lambda_ / 2, Nt)
 
     # Empty arrays
-    beam_t = np.zeros((np.shape(AoA)[0], N))
-    beam_r = np.zeros((np.shape(AoA)[0], N))
-    R = np.zeros((np.shape(AoA)[0], Nt, Nr, N))
+    beam_t = np.zeros((np.shape(AoA_Global)[0], N))
+    beam_r = np.zeros((np.shape(AoA_Global)[0], N))
+    R = np.zeros((np.shape(AoA_Global)[0], Nt, Nr, N))
+    AoA_Local = []
 
     # print("Calculating R")
     F = np.zeros((Nt, Nt), dtype=np.complex128)
@@ -80,12 +80,13 @@ if __name__ == "__main__":
 
     for q in range(Nr):
         W[q, :] = (1 / np.sqrt(Nr)) * np.exp(-1j * np.pi * np.arange(Nr) * ((2 * q - Nr) / Nr))
-    
-    for episode in range(np.shape(AoA)[0]):
-        for j in range(np.shape(AoA[episode][0])[0]):
+
+    for episode in range(np.shape(AoA_Global)[0]):
+        AoA_Local.append(helpers.get_local_angle(AoA_Global[episode][0], Orientation[episode][0][2, :]))
+        for j in range(np.shape(AoA_Global[episode][0])[0]):
             # print("Calculating H")
-            alpha_rx = helpers.steering_vectors2d(dir=-1, theta=AoA[episode][0][j, :], r=r_r, lambda_=lambda_)
-            alpha_tx = helpers.steering_vectors2d(dir=1, theta=AoD[episode][0][j, :], r=r_t, lambda_=lambda_)
+            alpha_rx = helpers.steering_vectors2d(dir=-1, theta=AoA_Local[episode][j, :], r=r_r, lambda_=lambda_)
+            alpha_tx = helpers.steering_vectors2d(dir=1, theta=AoD_Global[episode][0][j, :], r=r_t, lambda_=lambda_)
             beta = coeff[episode][0][j, :]
 
             H = np.zeros((Nr, Nt), dtype=np.complex128)
@@ -102,8 +103,8 @@ if __name__ == "__main__":
 
             # Equation 9 & 10 in "A Deep Learning Approach to Location"
             # Page 8
-            beam_t[episode,j] = np.arccos((2*index[0]-Nt)/Nt)*180/np.pi
-            beam_r[episode,j] = 180 - np.arccos((2*index[1]-Nr)/Nr)*180/np.pi
+            beam_t[episode, j] = np.arccos((2*index[0]-Nt)/Nt)*180/np.pi
+            beam_r[episode, j] = 180 - np.arccos((2*index[1]-Nr)/Nr)*180/np.pi
             # OLD: np.arccos(1-(2*index[1]/(Nr-1)))*180/np.pi
 
             # print(f"Reiver beam dir: {beam_r}\nTransmitter beam dir: {beam_t}")
@@ -117,35 +118,25 @@ if __name__ == "__main__":
     helpers.plot_directivity(F, 1000, "Transmitter")
 
     # Plot the beam direction for the receiver and transmitter
-    for i in range(np.shape(AoA[0][0])[0]):
-        if i > 3:
-            if beam_r[0, i-2] > 90:
-                if beam_r[0, i-1] >= 90:
-                    if beam_r[0, i] == 0:
-                        beam_r[0, i] = 180
-            else:
-                if beam_r[0, i-1] <= 90:
-                    if beam_r[0, i] == 180:
-                        beam_r[0, i] = 0
+    # Caculate the Line Of Sight angle for transmitter in GLOBAL coordinate system
+    AoA_LOS_t = np.abs(np.arctan2(pos_log[0][1], pos_log[0][0]) * 180/np.pi)
+
+    # Caculate the Line Of Sight angle for receiver in GLOBAL coordinate system
+    AoA_LOS_r_GLOBAL = np.arctan2(-pos_log[0][1], -pos_log[0][0])
+    AoA_LOS_r_GLOBAL.shape = (N, 1)
+
+    # Caculate the Line Of Sight angle for receiver in LOCAL coordinate system
+    AoA_LOS_r_LOCAL = helpers.get_local_angle(AoA_LOS_r_GLOBAL[0][0], Orientation[0][0][2, :]) * 180/np.pi
 
     plt.figure()
     plt.title("Receiver")
-    plt.plot(beam_r[0,:])
-    plt.plot(np.abs(180-AoA_L))
-
-    for i in range(np.shape(AoA[0][0])[0]):
-        if i > 2:
-            if beam_t[0, i-1] > 90:
-                if beam_t[0, i] == 0:
-                    beam_t[0, i] = 180
-            else:
-                if beam_t[0, i] == 180:
-                    beam_t[0, i] = 0
+    plt.plot(beam_r[0, :])
+    plt.plot(np.abs(AoA_LOS_r_LOCAL))
 
     plt.figure()
     plt.title("Transmitter")
-    plt.plot(beam_t[0,:])
-    plt.plot(AoA_L)
+    plt.plot(beam_t[0, :])
+    plt.plot(AoA_LOS_t)
 
     fig, ax = plt.subplots()
     ax.set_title("Kunst")
