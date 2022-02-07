511a99eda827e711ad07c5f062c4f5ba76f34ab0
Peter_Fisker <pet5631@gmail.com>
2022-02-07 13:43:10 +0100

    Updated the script to support octave again.

--- Quadriga/get_data.m
+++ Quadriga/get_data.m
@@ -1,99 +1,109 @@
 
 function done = get_data(fc, pos_log, name, ENGINE, scenarios)
+
+if ENGINE == "octave"
+    mkdir 'Data_sets\tmp'
+else
     rng shuffle
-    
-    load("Data_sets/"+pos_log);
-    
-    l = qd_layout;                                          % Create new QuaDRIGa layout
-    l.simpar.center_frequency = fc;                         % Set center frequency to 2 GHz
-    l.simpar.use_absolute_delays = 1;                       % Enables true LOS delay
-    l.simpar.show_progress_bars = 1;                        % Disable progress bars
-    l.tx_position = [0, 0, 10]';                            % Set BS posittions
-    
-    l.rx_track = qd_track();
-    
-    sp = size(pos_log);  
-    sc = size(scenarios);
-
     mkdir("Data_sets\tmp")
+end
 
-    chunksize = 20000;
-    for episode = 1:sp(1)
-        sce_index = randi([1, sc(1)]);
-        l.rx_track.scenario = strtrim(scenarios(sce_index, :));             % Set propagation scenario
-        for chunk = 1:ceil(sp(3)/chunksize)
-            if chunk*chunksize>sp(3)
-                end_idx = sp(3);
-            else
-                end_idx = chunk*chunksize;
-            end
-            l.rx_track.positions=squeeze(pos_log(episode, :, (chunk-1)*chunksize + 1:end_idx));   % Set start position and MT height
+filepath = strcat("Data_sets/",pos_log);
+load(filepath);
+% load("Data_sets/"+pos_log);
 
-            
-            l.rx_track.calc_orientation();
-    
-            l.rx_track.no_segments = l.rx_track.no_snapshots;       % Use spatial consisteny for mobility
-    
-            b = l.init_builder;                                     % Initializes channel builder
-    
-%             b.gen_parameters(0);                            % Clears LSF SSF and SOS parameters
-            b.gen_parameters(5);                            % Generates all missing parameters
-        
-            c = get_channels( b );                          % Generate channel coefficients
-            c = merge( c, [], 0 );                          % Combine output channels
-            c.individual_delays = 0;                        % Remove per-antenna delays
+l = qd_layout;                                          % Create new QuaDRIGa layout
+l.simpar.center_frequency = fc;                         % Set center frequency to 2 GHz
+l.simpar.use_absolute_delays = 1;                       % Enables true LOS delay
+l.simpar.show_progress_bars = 1;                        % Disable progress bars
+l.tx_position = [0, 0, 10]';                            % Set BS posittions
 
-            output = cell(4,1);
-            output{1} = b.AoA;
-            output{2} = b.AoD;
-            output{3} = squeeze(c.coeff(1,1,:,:))';
-            output{4} = l.rx_track.orientation();
-        
-            if ENGINE == "octave"
-                save("-7", "Data_sets/tmp/"+name+"_"+string(episode)+"_"+string(chunk), 'output');
-            else
-                save("Data_sets/tmp/"+name+"_"+string(episode)+"_"+string(chunk), 'output');
-            end
+l.rx_track = qd_track();
 
-        end 
-    end
+sp = size(pos_log);
+sc = size(scenarios);
 
-    AoA_cell = cell(sp(1),1);
-    AoD_cell = cell(sp(1),1);
-    Coeff_cell = cell(sp(1),1);
-    Ori_cell = cell(sp(1), 1);
+% mkdir("Data_sets\tmp")
 
-    for episode = 1:sp(1)
-        AoA = [];
-        AoD = [];
-        Coeff = [];
-        Ori = [];
-        for chunk = 1:ceil(sp(3)/chunksize)
-            load("Data_sets/tmp/"+name+"_"+string(episode)+"_"+string(chunk));
-            AoA = [AoA; output{1}];
-            AoD = [AoD; output{2}];
-            Coeff = [Coeff; output{3}];
-            Ori = [Ori, output{4}];
+chunksize = 20000;
+for episode = 1:sp(1)
+    sce_index = randi([1, sc(1)]);
+    l.rx_track.scenario = strtrim(scenarios(sce_index, :));             % Set propagation scenario
+    for chunk = 1:ceil(sp(3)/chunksize)
+        if chunk*chunksize>sp(3)
+            end_idx = sp(3);
+        else
+            end_idx = chunk*chunksize;
         end
-        AoA_cell{episode} = AoA;
-        AoD_cell{episode} = AoD;
-        Coeff_cell{episode} = Coeff;
-        Ori_cell{episode} = Ori;
+        l.rx_track.positions=squeeze(pos_log(episode, :, (chunk-1)*chunksize + 1:end_idx));   % Set start position and MT height
+
+
+        l.rx_track.calc_orientation();
+
+        l.rx_track.no_segments = l.rx_track.no_snapshots;       % Use spatial consisteny for mobility
+
+        b = l.init_builder;                                     % Initializes channel builder
+
+        %             b.gen_parameters(0);                            % Clears LSF SSF and SOS parameters
+        b.gen_parameters(5);                            % Generates all missing parameters
+
+        c = get_channels( b );                          % Generate channel coefficients
+        c = merge( c, [], 0 );                          % Combine output channels
+        c.individual_delays = 0;                        % Remove per-antenna delays
+
+        output = cell(4,1);
+        output{1} = b.AoA;
+        output{2} = b.AoD;
+        output{3} = squeeze(c.coeff(1,1,:,:))';
+        output{4} = l.rx_track.orientation();
+
+        if ENGINE == "octave"
+            save("-7", "Data_sets/tmp/"+name+"_"+string(episode)+"_"+string(chunk), 'output');
+        else
+            save("Data_sets/tmp/"+name+"_"+string(episode)+"_"+string(chunk), 'output');
+        end
+
     end
+end
 
-    output = cell(4,1);
-    output{1} = AoA_cell;
-    output{2} = AoD_cell;
-    output{3} = Coeff_cell;
-    output{4} = Ori_cell;
+AoA_cell = cell(sp(1),1);
+AoD_cell = cell(sp(1),1);
+Coeff_cell = cell(sp(1),1);
+Ori_cell = cell(sp(1), 1);
 
-    if ENGINE == "octave"
-        save("-7", "Data_sets/" + name, 'output');
-    else
-        save("Data_sets/" + name, 'output');
+for episode = 1:sp(1)
+    AoA = [];
+    AoD = [];
+    Coeff = [];
+    Ori = [];
+    for chunk = 1:ceil(sp(3)/chunksize)
+        filepath = strcat("Data_sets/tmp/",name,"_",string(episode),"_",string(chunk));
+        load(filepath);
+%         load("Data_sets/tmp/"+name+"_"+string(episode)+"_"+string(chunk));
+        AoA = [AoA; output{1}];
+        AoD = [AoD; output{2}];
+        Coeff = [Coeff; output{3}];
+        Ori = [Ori, output{4}];
     end
-    % rmdir("Data_sets/tmp", 's')
+    AoA_cell{episode} = AoA;
+    AoD_cell{episode} = AoD;
+    Coeff_cell{episode} = Coeff;
+    Ori_cell{episode} = Ori;
+end
 
-    done = 1;
-    
+output = cell(4,1);
+output{1} = AoA_cell;
+output{2} = AoD_cell;
+output{3} = Coeff_cell;
+output{4} = Ori_cell;
+
+if ENGINE == "octave"
+    save("-7", "Data_sets/" + name, 'output');
+else
+    save("Data_sets/" + name, 'output');
+end
+% rmdir("Data_sets/tmp", 's')
+
+done = 1;
+
 end
