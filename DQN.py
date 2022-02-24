dfaa04a276c4523c8adfea0e32680daea71fe984
Nicolai Almskou <almskoun@hotmail.com>
2022-02-24 09:35:01 +0100

    Updated draft

--- DQN.py
+++ DQN.py
@@ -8,7 +8,6 @@ and modified by:
 
 
 # %% ---------- Imports ----------
-import gym  # Environemnt is taken from this package
 import time
 import itertools
 import numpy as np
@@ -16,12 +15,19 @@ import tensorflow as tf
 from statistics import mean
 from collections import namedtuple
 
+import json
+import argparse
+from time import time
 
-from classes_DQN import Model, ReplayMemory, EpsilonGreedyStrategy, DQN_Agent
+import helpers
+from classes_DQN import Model, ReplayMemory, EpsilonGreedyStrategy, DQN_Agent, Environment
 
 # Initialize tensorboard object
-name = f'DQN_logs_{time.time()}'
+name = f'DQN_logs_{time()}'
 summary_writer = tf.summary.create_file_writer(logdir=f'logs/{name}/')
+
+# global parameters
+RUN = False
 
 
 # %% ---------- Functions ----------
@@ -33,6 +39,18 @@ def copy_weights(Copy_from, Copy_to):
     variables1 = Copy_to.trainable_variables
     for v1, v2 in zip(variables1, variables2):
         v1.assign(v2.numpy())
+
+
+def parser():
+    description = """Adaptive Beamforming using Reinforcement Learning"""
+    parser = argparse.ArgumentParser(description=description)
+
+    help_str = """Name of the .json file which contains your test parameters.
+                Default is the 'default.json' test parameters'"""
+    parser.add_argument('--test_par', type=str,
+                        default="default", help=help_str)
+
+    return parser.parse_args()
 
 
 # %% ---------- Main ----------
@@ -50,8 +68,147 @@ if __name__ == "__main__":
     epochs = 1000
     hidden_units = [200, 200]
 
+    # debug: [plot, print, savefig]
+    debug = [False, False, True]
+
+    # Parse arguments
+    args = parser()
+
+    # Load test_parameters configuration
+    with open(f'Test_parameters/{args.test_par}.json', 'r') as fp:
+        settings = json.load(fp)
+
+    # ----------- Channel Simulation Parameters -----------
+    # Name of the data file afte "data_" and "data_pos_"
+    FILENAME = settings["filename"]
+
+    # Name of the chosen case "pedestrian", "car_urban" or "car_highway"
+    CASE = settings["case"]
+
+    # which engine should be be used "octave" OR "MATLAB"
+    ENGINE = settings["engine"]
+
+    # Possible scenarios for Quadriga simulations
+    scenarios = settings["sim_par"]["scenarios"]
+
+    # Number of steps in a episode
+    N = settings["sim_par"]["N_steps"]
+
+    # Sample Period [s]
+    sample_period = settings["sim_par"]["sample_period"]
+
+    # Number of episodes
+    M = settings["sim_par"]["M_episodes"]
+
+    # Multi user
+    multi_user = settings["multi_user"]
+
+    # Number of base stations
+    Nbs = 4
+
+    # Radius for communication range [m]
+    r_lim = settings["sim_par"]["rlim"]
+
+    # Intersite distance between base stations [m]
+    intersite = settings["sim_par"]["intersite"]
+
+    # ----------- Reinforcement Learning Parameters -----------
+    # State parameters
+    n_actions = 3
+    n_ori = 3
+
+    dist_res = 8
+    angle_res = 8
+
+    # Chunk size, number of samples taken out.
+    chunksize = settings["test_par"]["chunk_size"]
+
+    # Number of episodes per chunk
+    Episodes = settings["test_par"]["episodes"]
+
+    # Which method RL should us: "simple", "SARSA" OR "Q-LEARNING"
+    METHOD = settings["RL_par"]["method"]
+
+    # ----------- Extracting variables from case -----------
+    # Load Scenario configuration
+    with open(f'Cases/{CASE}.json', 'r') as fp:
+        case = json.load(fp)
+
+    # Number of antennae
+    Nt = case["transmitter"]["antennea"]  # Transmitter
+    Nr = case["receiver"]["antennea"]  # Receiver
+
+    # Number of beams
+    Nbt = case["transmitter"]["beams"]  # Transmitter
+    Nbr = case["receiver"]["beams"]  # Receiver
+
+    fc = case["fc"]  # Center frequency
+    lambda_ = 3e8 / fc  # Wave length
+    P_t = case["P_t"]  # Transmission power
+
+    # ----------- Create the data -----------
+    # Take time on how long it takes to run the simulation / load data in
+    t_start = time()
+    # Load or create the data
+    channel_par, pos_log = helpers.get_data(RUN, ENGINE, case, multi_user,
+                                            f"data_pos_{FILENAME}.mat", f"data_{FILENAME}",
+                                            [fc, N, M, r_lim, intersite, sample_period, scenarios, debug])
+
+    print(f"Channel parameters generation took: {time() - t_start}.3f seconds", flush=True)
+
+    # Take time on how long it take to the run the RL part
+    t_start = time()
+
+    # First entry are the BS coordinates
+    if len(pos_log[0]) == 1:
+        pos_bs = pos_log[0][0]
+    else:
+        pos_bs = pos_log[0]
+
+    # Removes the BS from the pos_log
+    pos_log = pos_log[1:]
+
+    # Re-affirm that "M" matches data
+    M = len(pos_log)
+
+    # ----------- Extract data from Quadriga simulation -----------
+    AoA_Global = channel_par[0][0]  # Angle of Arrival in Global coord. system
+    AoD_Global = channel_par[1][0]  # Angle of Departure in Global coord. system
+    coeff = channel_par[2][0]  # Channel Coefficients
+    Orientation = channel_par[3][0]  # Orientation in Global coord. system
+
+    if CASE == 'pedestrian':
+        # Add some random noise to the orientation to simulate a moving person
+        Orientation = helpers.noisy_ori(Orientation)
+
+    # ----------- Prepare the simulation - Channel -----------
+    # Make ULA antenna positions - Transmitter
+    r_r = np.zeros((2, Nr))
+    r_r[0, :] = np.linspace(0, (Nr - 1) * lambda_ / 2, Nr)
+
+    # Make ULA antenna positions - Receiver
+    r_t = np.zeros((2, Nt))
+    r_t[0, :] = np.linspace(0, (Nt - 1) * lambda_ / 2, Nt)
+
+    # Preallocate empty arrays
+    beam_t = np.zeros((M, N))
+    beam_r = np.zeros((M, N))
+    AoA_Local = []
+
+    # Calculate DFT-codebook - Transmitter
+    F = helpers.codebook(Nbt, Nt)
+
+    # Calculate DFT-codebook - Receiver
+    W = helpers.codebook(Nbr, Nr)
+
+    # Calculate the AoA in the local coordinate system
+    for m in range(M):
+        AoA_Local.append(helpers.get_local_angle(AoA_Global[m][0], Orientation[m][0][2, :]))
+
+    # ----------- DQN -----------
     # Initialize the environment
-    env = gym.make('CartPole-v0')
+    env = Environment(W, F, Nt, Nr, Nbs, Nbt, Nbr,
+                      r_r, r_t, fc, P_t)
 
     """
     Notice that we are not using any function to make the states discrete here as DQN
@@ -78,7 +235,14 @@ if __name__ == "__main__":
     total_rewards = np.empty(epochs)
 
     for epoch in range(epochs):
-        state = env.reset()
+        # Choose data for the episode
+        data_idx = np.random.randint(0, N - chunksize) if (N - chunksize) else 0
+        path_idx = np.random.randint(0, M)
+
+        # Reset the environment with the new data
+        state = env.reset(AoA_Local[path_idx][:, data_idx:data_idx + chunksize],
+                          AoD_Global[path_idx][0][:, data_idx:data_idx + chunksize],
+                          coeff[path_idx][0][:, data_idx:data_idx + chunksize])
         ep_rewards = 0
         losses = []
 
--- classes_DQN.py
+++ classes_DQN.py
@@ -5,16 +5,15 @@ Created on Thu Feb 24 08:24:06 2022
 @author: almsk
 """
 
-
-# %% ---------- Imports ----------
 import math
 import random
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.layers as kl
 
+import helpers
 
-# %% ---------- Classes ----------
+
 class Model(tf.keras.Model):
     """
     Subclassing a multi-layered NN using Keras from Tensorflow
@@ -95,3 +94,123 @@ class DQN_Agent():
             return random.randrange(self.num_actions), rate, True
         else:
             return np.argmax(policy_net(np.atleast_2d(np.atleast_2d(state).astype('float32')))), rate, False
+
+
+class Environment():
+    """
+    Environment created from the QuaDRiga simulation
+    """
+
+    def __init__(self, W, F, Nt, Nr, Nbs, Nbt, Nbr,
+                 r_r, r_t, fc, P_t):
+        self.AoA = 0
+        self.AoD = 0
+        self.Beta = 0
+
+        # Codebooks
+        self.W = W
+        self.F = F
+
+        # Number of antennas
+        self.Nt = Nt
+        self.Nr = Nr
+
+        # Number of basestations
+        self.Nbs = Nbs
+
+        # Action space
+        self.action_space_n = Nbr
+
+        # Antenna Posistions
+        self.r_t = r_t
+        self.r_r = r_r
+
+        # Wavelength
+        self.lambda_ = 3e8 / fc
+
+        # Transmitted power
+        self.P_t = P_t
+
+        # Number of steps / step count
+        self.nstep = 0
+        self.stepnr = 0
+
+        # What state we are in
+        self.state = np.random.choice(range(self.action_space_n), size=3)
+
+    def _get_reward(self, action):
+
+        # Empty Reward Matrix
+        R = np.zeros((self.Nbs, len(self.F[:, 0]), len(self.W[:, 0])))
+
+        # Calculate the reward pairs for each base station
+        for b in range(self.Nbs):
+            # Calculate steering vectors for transmitter and receiver
+            alpha_rx = helpers.steering_vectors2d(direction=-1, theta=self.AoA[b, self.stepnr, :],
+                                                  r=self.r_r, lambda_=self.lambda_)
+            alpha_tx = helpers.steering_vectors2d(direction=1, theta=self.AoD[b, self.stepnr, :],
+                                                  r=self.r_t, lambda_=self.lambda_)
+            # Calculate channel matrix H
+            H = np.zeros((self.Nr, self.Nt), dtype=np.complex128)
+            for i in range(len(self.Beta[b, self.stepnr, :])):
+                H += self.Beta[b, self.stepnr, i] * (alpha_rx[i].T @ np.conjugate(alpha_tx[i]))
+            H = H * np.sqrt(self.Nr * self.Nt)
+
+            # Calculate the reward
+            for p in range(len(self.F[:, 0])):  # p - transmitter
+                for q in range(len(self.W[:, 0])):  # q - receiver
+                    R[b, p, q] = np.linalg.norm(np.sqrt(self.P_t) * np.conjugate(self.W[q, :]).T
+                                                @ H @ self.F[p, :]) ** 2
+
+        return np.max(R[:, :, action]), np.max(R), np.min(np.max(R, axis=1)), np.mean(np.max(R, axis=1))
+
+    def _start_state(self):
+
+        # Select a random start state
+        self.state = np.random.choice(range(self.action_space_n), size=3)
+
+        return self.state
+
+    def _start_update(self, action):
+
+        # Insert new action in the front of the array
+        state_tmp = np.insert(self.state, 0, action)
+
+        # Remove the last element so keep the same state dim.
+        self.state = np.delete(state_tmp, -1)
+
+        return self.state
+
+    def step(self, action):
+
+        # Get the reward
+        reward, _, _, _ = self._get_reward(action)
+
+        # Get the next_state
+        next_state = self._state_update(action)
+
+        # Update counter and see if episode if finished
+        self.stepnr += 1
+        if self.stepnr == self.nstep - 1:
+            done = True
+        else:
+            done = False
+
+        return next_state, reward, done
+
+        # return reward, max_reward, min_reward, mean_reward
+
+    def reset(self, AoA, AoD, Beta):
+        # Reset step counter
+        self.stepnr = 0
+
+        # Get number of steps in the episode
+        self.nstep = np.size(AoA)  # TODO: need to be checked
+
+        # Save updated variables
+        self.AoA = AoA
+        self.AoD = AoD
+        self.Beta = Beta
+
+        # Return the start state()
+        return self._start_state()
